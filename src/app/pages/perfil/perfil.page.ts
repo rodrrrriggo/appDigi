137403e5fc707707161389e64194d65dd72124bb
rorro <rodguzm03@gmail.com>
2024-09-01 19:18:36 -0400

    cambios a redirecciones y scss

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-perfil',
@@ -8,17 +9,42 @@ import { AlertController } from '@ionic/angular';
 })
 export class PerfilPage implements OnInit {
 
-  ngOnInit() {
-  }
-  constructor(private alertController: AlertController) {}
+  nombre: string = "";
+  correo: string = "";
+  contrasena: string = "";
+
+  constructor(
+    public alertController: AlertController, private toastController: ToastController, private router: Router) {}
+
+  ngOnInit() {}
 
   async presentAlert() {
     const alert = await this.alertController.create({
       header: 'DIGIGAMES DICE:',
-      message: 'Perfil actualizado con exito!',
+      message: 'Perfin Modificado!',
       buttons: ['Continuar'],
     });
 
     await alert.present();
+  }
+
+  async ingresoUsuarios() {
+    if (this.nombre === "" || this.correo === "" || this.contrasena === "") {
+      await this.presentToast('middle', 'Faltan campos por modificar!');
+      return;
+    } else {
+      await this.presentAlert();
+      this.router.navigate(['/catalogo']);
+    }
+  }
+
+  async presentToast(position: 'middle', texto: string) {
+    const toast = await this.toastController.create({
+      position: position,
+      message: texto,
+      duration: 2000,
+    });
+
+    await toast.present();
   }
 }
