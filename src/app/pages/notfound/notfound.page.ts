8dc1ee8b9390f70fec0cc41dc1a01c0f5f805614
rorro <rodguzm03@gmail.com>
2024-09-02 22:21:57 -0400

    cambios a notfound

--- src/app/pages/notfound/notfound.page.ts
+++ src/app/pages/notfound/notfound.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-notfound',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotfoundPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'DIGIGAMES DICE:',
+      message: 'Bienvenido nuevamente',
+      buttons: ['Comenzar'],
+    });
+
+    await alert.present();
   }
 
 }
